87d4117562fcbe871a868c485e01fe924999bf76
Cagri Zeki <cagrizeki@gmail.com>
2024-01-28 16:25:46 +0300

    Stars gained movement

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -8,6 +8,7 @@ import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import { TextureLoader } from "three";
+import Cag from "./MovingStars";
 
 export function Earth(props) {
   const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
@@ -28,14 +29,15 @@ export function Earth(props) {
   return (
     <>
       <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
-      <Stars
+     {/*  <Stars
         radius={300}
         depth={60}
         count={20000}
         factor={10}
         saturation={0}
         fade={true}
-      />
+      /> */}
+      <Cag></Cag>
       <mesh ref={cloudsRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
